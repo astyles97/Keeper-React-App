8cf8d34392ecabd1a6a8bf904d167393beb69ccd
Adam S <113198893+astyles97@users.noreply.github.com>
2023-04-02 12:55:06 -0400

    A simple note app made with React.
    A React app from Angela Yu's course. It is made utilizing React, React-DOM, conditional rendering, complex state, and using jsx files.

---
+++ Keeper-React-App/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TungstenIcon from "@mui/icons-material/Tungsten";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <TungstenIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
